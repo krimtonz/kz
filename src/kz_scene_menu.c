658d5fca247227efd91fbde820f2b0dcfe00131e
KrimtonZ <krimtonz@gmail.com>
2019-12-04 12:15:41 -0600

    fix crash when loading currently loading room, change room loading number to number input

--- src/kz_scene_menu.c
+++ src/kz_scene_menu.c
@@ -38,30 +38,14 @@ static int unload_room(struct menu_item *item, enum menu_callback callback, void
 
 static int load_room(struct menu_item *item, enum menu_callback callback, void *data){
     if(callback == MENU_CALLBACK_ACTIVATE){
+        if(room_idx == z2_game.room_ctx.rooms[0].idx) return unload_room(item,callback,data);
+        if(room_idx >= z2_game.room_cnt) return 1;
         z2_game.room_ctx.rooms[0].idx = room_idx;
         z2_game.room_ctx.rooms[0].file = NULL;
         z2_unloadroom(&z2_game,&z2_game.room_ctx);
         z2_game.room_ctx.rooms[0].idx=room_idx;
         z2_game.room_ctx.rooms[0].file = NULL;
         z2_loadroom(&z2_game,&z2_game.room_ctx,room_idx);
-        return 1;
-    }
-    return 0;
-}
-
-static int room_dec(struct menu_item *item, enum menu_callback callback, void *data){
-    if(callback == MENU_CALLBACK_ACTIVATE){
-        room_idx += z2_game.room_cnt - 1;
-        room_idx %= z2_game.room_cnt;
-        return 1;
-    }
-    return 0;
-}
-
-static int room_inc(struct menu_item *item, enum menu_callback callback, void *data){
-    if(callback == MENU_CALLBACK_ACTIVATE){
-        room_idx++;
-        room_idx %= z2_game.room_cnt;
         return 1;
     }
     return 0;
@@ -94,12 +78,7 @@ struct menu *create_scene_menu(){
 
     menu_add_button(&scene,0,6,"unload room",unload_room,NULL);
     menu_add_button(&scene,0,7,"load room",load_room,NULL);
-    static watch_t room_watch;
-    room_watch.address = &room_idx;
-    room_watch.type = WATCH_TYPE_S32;
-    room_watch.floating=0;
-    menu_add_button(&scene,10,7,"-",room_dec,NULL);
-    menu_add_watch(&scene,11,7,&room_watch,1);
-    menu_add_button(&scene,12,7,"+",room_inc,NULL);
+    menu_add_number_input(&scene,13,7,NULL,NULL,10,2,&room_idx,4);
+
     return &scene;
 }
